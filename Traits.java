fa75f7d5014d429f93cf1874bc22029cd0a0a33e
jesuskunq <97258149+jesuskunq@users.noreply.github.com>
2022-06-26 04:50:59 -0400

    Create Traits.java

---
+++ Traits.java
@@ -0,0 +1,109 @@
+public class Traits{
+  
+  public static String assignFirstLetter(String letter){
+    if(letter.equals("a")  || letter.equals("b") || letter.equals("c") || letter.equals("d") || letter.equals("e")){
+      return "ENFJ / Protaganist";
+    } 
+    else if (letter.equals("f")  || letter.equals("g")  || letter.equals("h")  || letter.equals("i")  || letter.equals("j")){
+      return "INTJ / Architect";
+    }
+    else if (letter.equals("l")  || letter.equals( "m" )|| letter.equals("n")  || letter.equals("o")  || letter.equals("p")){
+      return "ENFP / Campaigner";
+    }
+    else if (letter.equals("q")  || letter.equals("r")  || letter.equals("s")  || letter.equals("t") || letter.equals("u")){
+      return "ESFJ / Consul"; 
+    }
+    else if (letter.equals("v")  || letter.equals("w")  || letter.equals("x")  || letter.equals("y")  || letter.equals("z")  || letter.equals("k")){
+      return "ISFJ / Defender";
+    }
+    else{
+      return " ";
+    }
+  }
+
+  
+    public static String assignFirstTrait(String letter){
+    if(letter.equals("a")  || letter.equals("b")  || letter.equals("c") || letter.equals("d")|| letter.equals("e")){
+      return "ENFJ are born leaders who have strong ideas and values and find enjoyment in guiding friends; an example is Padme Amidala from Star Wars";
+    } 
+    else if (letter.equals("f") || letter.equals("g")  || letter.equals("h")  || letter.equals("i")  || letter.equals("j")){
+      return "INTJ are highly independent, confident, and ambitious who solve the world's problems through methodical thinking. A sci-fi character who has INTJ personality type is Batman.";
+    }
+    else if (letter.equals("l")  || letter.equals("m") || letter.equals("n")  || letter.equals("o")  || letter.equals("p")){
+      return "ENFP are open-hearted and outgoing and reflect their lively spirits to everyone around them. One example is Spiderman.";
+    }
+    else if (letter.equals("q")  || letter.equals("r")  || letter.equals("s")  || letter.equals("t")  || letter.equals("u")){
+      return "ESFJ are gregarious and attentive people who care about giving back to the community. One character is Star Wars smuggler Lando Calrissian."; 
+    }
+    else if (letter.equals("v")  || letter.equals("w")  || letter.equals("x")  || letter.equals("y")  || letter.equals("z")  || letter.equals("k")){
+      return "ISFJ tend to be responsible and devoted people who achieve beyond perfection. A character includes Hawkeye from the MCU";
+    }
+      else{
+        return " ";
+      }
+  }
+
+
+
+
+
+  
+  public static String assignLastLetter(String letter){
+    if(letter.equals("a")  || letter.equals("b")  || letter.equals("c") || letter.equals("d") || letter.equals("e")){
+      return "ESFP / Entetainer";
+    } 
+    else if (letter.equals("f")  || letter.equals("g")  || letter.equals("h")  || letter.equals("i") || letter.equals("j")){
+      return "ISTP / Virtuoso";
+    }
+    else if (letter.equals("l")  || letter.equals("m")  || letter.equals("n")  || letter.equals("o")  || letter.equals("p")){
+      return "ENTP / Debater";
+    }
+    else if (letter.equals("q")  || letter.equals("r")  || letter.equals("s")  || letter.equals("t")  || letter.equals("u")){
+      return "ISFP / Adventurer"; 
+    }
+    else if (letter.equals("v")  || letter.equals("w")  || letter.equals("x")  || letter.equals("y")  || letter.equals("z")  || letter.equals("k")){
+      return "ESTP / Entrepeneur";
+    }
+    else {
+      return" ";
+    }
+  }
+
+
+
+
+
+
+  
+  public static String assignLastTrait(String letter){
+    if(letter.equals("a")  || letter.equals("b")  || letter.equals("c") || letter.equals("d") || letter.equals("e") ){
+      return "ESFP is very Spontaneous and are natural born entertainers. They are often very social and tries to encourage others into joining them in these activies. One character that is a ESFP is Seong Gi-hun from Squid Game, he is the main character that wins the game and earns the money";
+    } 
+    else if (letter.equals("f")  || letter.equals("g")  || letter.equals("h")  || letter.equals("i")  || letter.equals("j") ){
+      return "ISTP are usually virtuoso; they usually have a individualistic mindset, chasing after their goals that don't require much help from others. One character that is a ISTP is Han Solo from Star Wars";
+    }
+    else if (letter.equals("l")  || letter.equals("m")  || letter.equals("n")  || letter.equals("o")  || letter.equals("p") ){
+      return "ENTP are quick-witted people who is not scared to defy the status quo and argue what they believe is right. One character similar to this is Tony Stark, aka Iron Man";
+    }
+    else if (letter.equals("q")  || letter.equals("r")  || letter.equals("s")  || letter.equals("t")  || letter.equals("u") ){
+      return "ISFP are introverts who have an open mind when they see life or have new experiences, they are often driven by curiosity and are willing to try anything that peaks their interest. One character like this is Thea Queen from Arrow"; 
+    }
+    else if (letter.equals("v")  || letter.equals("w")  || letter.equals("x")  || letter.equals("y")  || letter.equals("z")  || letter.equals("k") ){
+      return "ESTP are Entrepreneurs who are energetic and almost always at the center of attention. They are very action-oriented. Someone like this is Hank Schrader from Breaking Bad";
+    }
+    else {
+      return " ";
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+  
+}
